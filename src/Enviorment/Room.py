1e96bca6613c745f337fd7120c4e6e1160538551
elijah6606-byte <elijah6606@gmail.com>
2025-11-17 18:32:58 -0500

    Refactor Room class constructor and fix typos
    Fixed typos and improved the constructor for the Room class, schedule assigning for rooms now as well I think is a good option, we will need a seperate file called idk simulation anything close works.

--- src/Enviorment/Room.py
+++ src/Enviorment/Room.py
@@ -1,31 +1,41 @@
-import ActorMain as ActorMain
+import Actor_Main as ActorMain # Retained original import, though not strictly needed here
 import random
-from typing import List, Any, Optinal, Iterable 
+from typing import List, Any, Optional 
 
 class Room:
     
-    """
-    Represents a room in the environment.
-    Args:
 
-        None
-    """
-
+    def __init__(self, name: str, description: str, actors: Optional[List[Any]] = None): 
         
-    def__init__(self, name: str, description: str, actors: Optional[]): 
         self.name = name
-        self.description = descriprtion
-        self. exits = {}
-        self.actor
-    
-
+        self.description = description
+        
+        
+        self.actors = actors if actors is not None else []
         self.items = []
         print(f"Room '{self.name}' created.")
+        
+    def add_actor(self, actor: Any):
 
+        if actor not in self.actors:
+            self.actors.append(actor)
+            print(f"{actor.name} has entered {self.name}.")
+            
+    def remove_actor(self, actor: Any):
 
+        if actor in self.actors:
+            self.actors.remove(actor)
+            print(f"{actor.name} has left {self.name}.")
+            
     
-
-
-    #TODO - Implement Room class
-    print("Room class is under construction")
+    def __str__(self):
+        
+        description = [f"You are in the **{self.name}**."]
+        description.append(self.description)
+        
+        if self.actors:
+            actor_names = [a.name for a in self.actors if hasattr(a, 'name')]
+            description.append(f"You see the following people here: {', '.join(actor_names)}.")
+            
+        return "\n".join(description)
     
